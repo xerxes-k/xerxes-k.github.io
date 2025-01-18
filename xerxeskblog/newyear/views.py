873545a7bf746bdada6c8e3d1e2c58a70f607fc7
xerxes-k <mluchk@gmail.com>
2025-01-18 23:44:42 +0900

    django newyear

--- xerxeskblog/newyear/views.py
+++ xerxeskblog/newyear/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from datetime import datetime
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse("<h1 style='color:red'> newyear <h1>")
-    # return render(request, 'tester/index.html')
+    now = datetime.now()
+    # return HttpResponse("<h1 style='color:red'> newyear <h1>")
+    return render(request, 'newyear/index.html', {
+        "newyear" : now.month == 1 and now.day == 1
+    })
