b87f812ccaff70a65f21cad5577609bde7ea43ce
xerxes-k <mluchk@gmail.com>
2025-01-22 22:30:20 +0900

    20250122

--- xerxeskblog/tasks/views.py
+++ xerxeskblog/tasks/views.py
@@ -1,18 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect
 from django import forms
+from django.urls import reverse
 
 class NewTaskForm(forms.Form):
     task = forms.CharField(label='new task')
 
-tasks = ['tester', 'pppppy', 'lecture3']
-
 def index(request):
+    if 'tasks' not in request.session:
+        request.session['tasks'] = []
     return render(request, 'tasks/index.html', {
-        'tasks':tasks,
+        'tasks':request.session['tasks'],
     })
 
 # Add a new task:
 def add(request):
+    if request.method == 'POST':
+        form = NewTaskForm(request.POST)
+        if form.is_valid():
+            task = form.cleaned_data['task']
+            request.session['tasks'] += task
+            return HttpResponseRedirect(reverse('tasks:index'))
+        else:
+            # if the form is invalid giving it back with the existing information
+            return render(request, "tasks/add.html", {
+                'form':form
+            })
+    
     return render(request, "tasks/add.html", {
         'form':NewTaskForm()
     })
+    
